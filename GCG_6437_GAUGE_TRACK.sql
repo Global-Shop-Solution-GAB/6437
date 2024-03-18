d32c2f6eefc50adce65daf3dd29ed7f44a1ae8de
Service Web <sw@gssmail.com>
2024-03-18 16:01:28 -0500

    Commit changes in C:\Websites\Git\emarlow\6437\GCG_6437_GAUGE_TRACK.sql

---
+++ GCG_6437_GAUGE_TRACK.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "GCG_6437_GAUGE_TRACK" (
+ "Gauge_SN" CHAR(50),
+ "Desc_1" CHAR(100),
+ "Desc_2" CHAR(100),
+ "Desc_3" CHAR(100),
+ "WO" CHAR(6),
+ "WO_Suff" CHAR(3),
+ "WO_Seq" CHAR(6),
+ "Cal_Date" CHAR(8),
+ "Cal_Due" CHAR(8),
+ "Iss_Date" CHAR(8),
+ "Iss_Time" CHAR(8),
+ "Iss_By" CHAR(8),
+ "Gauge_Owner" CHAR(10) );
