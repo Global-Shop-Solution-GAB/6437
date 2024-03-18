dc65a6cb3ad8e1abde2556bb406ea7a84f7aa314
Service Web <sw@gssmail.com>
2024-03-18 16:01:28 -0500

    Commit changes in C:\Websites\Git\emarlow\6437\GCG_6437_GAUGE_INFO.sql

---
+++ GCG_6437_GAUGE_INFO.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_6437_GAUGE_INFO" (
+ "ControlNum" CHAR(50),
+ "CalDate" DATE,
+ "Description" CHAR(100),
+ "Location" CHAR(50),
+ "NextCalDate" DATE,
+ "TDescription" CHAR(100),
+ "Manufacturer" CHAR(50),
+ "Model" CHAR(50) );
